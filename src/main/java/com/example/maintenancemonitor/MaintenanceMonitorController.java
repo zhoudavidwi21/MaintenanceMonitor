9734849ad0f5370a7dd2dbdc96c455586bdbe4e5
David <david281100@hotmail.com>
2022-06-16 13:26:36 +0200

    Added setMessage

--- src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
+++ src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
@@ -3,6 +3,7 @@ package com.example.maintenancemonitor;
 import com.sun.tools.javac.Main;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +21,10 @@ public class MaintenanceMonitorController {
     @RequestMapping("/getMessage")
     public String getMessage(){
         return maintenanceMonitor.getMessage();
+    }
+
+    @RequestMapping("/setMessage")
+    public void setStatus(@RequestParam String message){
+        maintenanceMonitor.setMessage(message);
     }
 }
