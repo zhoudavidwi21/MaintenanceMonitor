5adae390c351637bf98c79502d3fb6df75e016e6
David <david281100@hotmail.com>
2022-06-16 13:18:05 +0200

    Added basic functions (getStatus, getMessage), implemented according class

---
+++ src/main/java/com/example/maintenancemonitor/MaintenanceMonitor.java
@@ -0,0 +1,14 @@
+package com.example.maintenancemonitor;
+
+public class MaintenanceMonitor {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
@@ -0,0 +1,24 @@
+package com.example.maintenancemonitor;
+
+import com.sun.tools.javac.Main;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class MaintenanceMonitorController {
+
+    MaintenanceMonitor maintenanceMonitor = new MaintenanceMonitor();
+
+    @RequestMapping("/status")
+    public boolean getStatus()
+    {
+        return true;
+    }
+
+    @RequestMapping("/getMessage")
+    public String getMessage(){
+        return maintenanceMonitor.getMessage();
+    }
+}
